4355a492e44195c6bb91f9b108052d86104bf0c0
Taylor Collins <tchockey92@gmail.com>
2017-10-22 12:26:14 -0400

    - Added unit test for executing parallel requests. - Refactored original test to use new execution method.

--- CookBook.Test/CookbookApiPerformanceTest.cs
+++ CookBook.Test/CookbookApiPerformanceTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace CookBook.Test
@@ -9,25 +10,41 @@ namespace CookBook.Test
     public class CookbookApiPerformanceTest
     {
         [Fact]
-        public void AverageResponseTime_100Requests()
+        public void AverageResponseTime_100Requests_1MaxParallel()
         {
-            var allResponseTimes = new List<(DateTime Start, DateTime End)>();
-            
-            for (var i = 0; i < 100; i++)
+            var expected = 8;
+            var actual = ExecuteParallelRequests("http://localhost:5000/api/cookbook", 100, 1);
+            Assert.True(actual <= expected, $"Expected average response time of less than or equal to {expected} ms but was {actual} ms.");
+        }
+
+        [Fact]
+        public void AverageResponseTime_100Requests_2MaxParallel()
+        {
+            var expected = 8;
+            var actual = ExecuteParallelRequests("http://localhost:5000/api/cookbook", 100, 2);
+            Assert.True(actual <= expected, $"Expected average response time of less than or equal to {expected} ms but was {actual} ms.");
+        }
+
+        private double ExecuteParallelRequests(string url, int totalNumberOfRequests, int maxDegreeOfParallelism = -1)
+        {
+            var results = new double[totalNumberOfRequests];
+            var options = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = maxDegreeOfParallelism
+            };
+            var loopResult = Parallel.For(0, totalNumberOfRequests, options, (i) =>
             {
                 using (var client = new HttpClient())
                 {
-                    var start = DateTime.Now;
-                    var response = client.GetAsync("http://localhost:5000/api/cookbook").Result;
-                    var end = DateTime.Now;
+                    var startTime = DateTime.Now;
+                    var response = client.GetAsync(url).Result;
+                    var endTime = DateTime.Now;
 
-                    allResponseTimes.Add((start, end));
+                    results[i] = ((endTime - startTime).TotalMilliseconds);
                 }
-            }
+            });
 
-            var expected = 8;
-            var actual = (int)allResponseTimes.Select(r => (r.End - r.Start).TotalMilliseconds).Average();
-            Assert.True(actual <= expected, $"Expected average response time of less than or equal to {expected} ms but was {actual} ms.");
+            return results.Average();
         }
     }
 }
