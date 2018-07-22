649a59cf09c57028d369a643d3e402c713bdff60
Taylor Collins <tchockey92@aol.com>
2018-07-22 13:12:28 -0400

    - Added new Decorator repository

---
+++ CookBook.DB.Redis/Repositories/CookbookRedisRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using CookBook.Model.Entities;
+using CookBook.Model.Repositories;
+
+namespace CookBook.DB.Redis.Repositories
+{
+    public class CookbookRedisRepository : ICookbookRepository
+    {
+        private readonly ICookbookRepository _parentRepository;
+        public CookbookRedisRepository(ICookbookRepository parentRepository)
+        {
+            _parentRepository = parentRepository;
+        }
+
+        public void Add(string name)
+        {
+            _parentRepository.Add(name);
+        }
+
+        public int Count()
+        {
+            return _parentRepository.Count();
+        }
+
+        public void Delete()
+        {
+            _parentRepository.Delete();
+        }
+
+        public ICollection<Cookbook> Get()
+        {
+            return _parentRepository.Get();
+        }
+
+        public Cookbook Get(string name)
+        {
+            return _parentRepository.Get(name);
+        }
+    }
+}
