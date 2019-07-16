d161e83b04198ecc311dd18216d70916638810db
Taylor Collins <tchockey92@aol.com>
2019-07-15 20:09:57 -0400

    Updated tests to write results to output

--- CookBook.Test/Tests/CookbookApiLoadTests.cs
+++ CookBook.Test/Tests/CookbookApiLoadTests.cs
@@ -1,11 +1,19 @@
 ﻿using CookBook.Test.Utilities;
+using System.Linq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace CookBook.Test.Tests
 {
     public class CookbookApiLoadTests
     {
         private const string _cookbookNamePrefix = "LoadTestCookbook";
+        private readonly ITestOutputHelper _outputHelper;
+
+        public CookbookApiLoadTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
 
         [Fact]
         public void AverageResponseTime_100Requests_2MaxParallel()
@@ -15,9 +23,10 @@ namespace CookBook.Test.Tests
 
             // Act
             var results = HttpHelper.ExecuteParallelRequests($"{CookbookTestDataSetup.CookbookApiUrl}/{_cookbookNamePrefix}_1", 100, 2);
+            OutputResults(results);
 
             // Assert
-            Assert.InRange(results.AverageTimeInMilliseconds, 0, 2);
+            Assert.InRange(results.AverageTimeInMilliseconds, 0, 1);
         }
 
         [Fact]
@@ -28,9 +37,10 @@ namespace CookBook.Test.Tests
 
             // Act
             var results = HttpHelper.ExecuteParallelRequests($"{CookbookTestDataSetup.CookbookApiUrl}/{_cookbookNamePrefix}_1", 100, 4);
+            OutputResults(results);
 
             // Assert
-            Assert.InRange(results.AverageTimeInMilliseconds, 0, 4);
+            Assert.InRange(results.AverageTimeInMilliseconds, 0, 1);
         }
 
         [Fact]
@@ -41,9 +51,35 @@ namespace CookBook.Test.Tests
 
             // Act
             var results = HttpHelper.ExecuteParallelRequests($"{CookbookTestDataSetup.CookbookApiUrl}/{_cookbookNamePrefix}_1", 100, 6);
+            OutputResults(results);
 
             // Assert
-            Assert.InRange(results.AverageTimeInMilliseconds, 0, 6);
+            Assert.InRange(results.AverageTimeInMilliseconds, 0, 1);
+        }
+
+        [Fact]
+        public void AverageResponseTime_100Requests_20MaxParallel()
+        {
+            // Arrange
+            CookbookTestDataSetup.CreateCookbooks(_cookbookNamePrefix);
+
+            // Act
+            var results = HttpHelper.ExecuteParallelRequests($"{CookbookTestDataSetup.CookbookApiUrl}/{_cookbookNamePrefix}_1", 100, 20);
+            OutputResults(results);
+
+            // Assert
+            Assert.InRange(results.AverageTimeInMilliseconds, 0, 1);
+        }
+
+        private void OutputResults(HttpResults results)
+        {
+            _outputHelper.WriteLine($"Http Result times:");
+            int count = 1;
+            foreach (var result in results.Results.OrderBy(r => r.Start))
+            {
+                _outputHelper.WriteLine($"{count}\t: {result.Start.ToString("HH:mm:ss:fff")} - {result.End.ToString("HH:mm:ss:fff")}");
+                count++;
+            }
         }
     }
 }
